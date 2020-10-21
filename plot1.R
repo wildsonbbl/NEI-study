5474fa6b1309c5458412bb9fd1df1fcac50bfa90
wildsonbbl <uioowill@gmail.com>
2020-10-21 12:35:06 -0300

    plot 1 finished

---
+++ plot1.R
@@ -0,0 +1,38 @@
+library(dplyr)
+#Dowloading data and organizing it
+datadir <- './data'
+neidataurl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
+neidatazip  <- './data/neidata.zip'
+neidatadir <- './data/neidata'
+
+if(!dir.exists(datadir)){
+        dir.create(datadir)
+}
+
+if(!file.exists(neidatazip)){
+        download.file(url = neidataurl,destfile = neidatazip,method = 'curl')
+}else if(!dir.exists(paths = neidatadir)){
+        dir.create(path = neidatadir)
+        unzip(zipfile = neidatazip,exdir = neidatadir)
+}
+
+
+#Reading the data into R
+datafilenames <- dir(neidatadir)
+filepaths<- list()
+for(fl in datafilenames){
+        column <- sub(pattern = '[.]rds$',replacement = '',x = fl)
+        filepaths[column] <- paste0(neidatadir,'/',fl)
+}
+
+neisrc <- readRDS(filepaths[[1]])
+neismry <- readRDS(filepaths[[2]])
+
+totalyear <- neismry %>% group_by(year) %>% summarise(total = sum(Emissions)) %>%
+        select(year,total)
+
+png('./plot1.png',width = 480,height = 480)
+with(totalyear, plot(x = year,y = total,type = 'p',
+                     main = 'Total emissions from 1999 to 2008',
+                     ylab = 'Total PM2.5 emission (ton)',pch = 19,))
+dev.off()
